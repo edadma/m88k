6af3052013e0057a957d1af043c9add024c2c4b7
Edward Adrian Maxedon <edadma@gmail.com>
2023-05-20 14:00:33 -0400

    RAM/ROM

--- shared/src/main/scala/io/github/edadma/m88k/Addressable.scala
+++ shared/src/main/scala/io/github/edadma/m88k/Addressable.scala
@@ -4,7 +4,7 @@ import scala.collection.{immutable, mutable}
 
 trait Addressable:
   def base: Long
-  def length: Long
+  def size: Long
   def readByte(addr: Long): Int
   def writeByte(addr: Long, data: Byte): Unit
 
@@ -13,7 +13,7 @@ class Memory(blocks: Addressable*) extends Addressable:
 
   val mem = blocks.sortBy(_.base).to(immutable.ArraySeq)
   val base = mem.head.base
-  val length = mem.last.base + mem.last.length - base
+  val size = mem.last.base + mem.last.size - base
 
   def block(addr: Long): Option[Addressable] =
     search(mem, addr, _ < _.base, _ == _.base) match
@@ -21,7 +21,7 @@ class Memory(blocks: Addressable*) extends Addressable:
       case Left(idx) =>
         val prec = mem(idx - 1)
 
-        if addr < prec.base + prec.length then Some(prec) else None
+        if addr < prec.base + prec.size then Some(prec) else None
       case Right(idx) => Some(mem(idx))
 
   private def badAddress: Nothing = sys.error("address not found")
@@ -30,20 +30,28 @@ class Memory(blocks: Addressable*) extends Addressable:
 
   def writeByte(addr: Long, data: Byte): Unit = block(addr) getOrElse badAddress writeByte (addr, data)
 
-class IndexedSeqAddressable(seq: mutable.IndexedSeq[Byte], val base: Long) extends Addressable:
+class RAM(val base: Long, val size: Long) extends Addressable:
   require(base >= 0, "base is negative")
-  require(seq.nonEmpty, "Addressable is empty")
+  require(0 <= size && size <= Int.MaxValue, "size out of range")
 
-  val length = seq.length
+  val seq = mutable.ArraySeq.fill(size.toInt)(0.asInstanceOf[Byte])
 
   def readByte(addr: Long): Int =
-    require(base <= addr && addr < base + length, "address out of range")
+    require(base <= addr && addr < base + size, "address out of range")
     seq((addr - base).toInt) & 0xff
 
   def writeByte(addr: Long, data: Byte): Unit =
-    require(base <= addr && addr < base + length, "address out of range")
+    require(base <= addr && addr < base + size, "address out of range")
     seq((addr - base).toInt) = data
 
-def RAM(base: Long, length: Long): Addressable =
-  require(0 < length && length <= Int.MaxValue, "length out of range")
-  new IndexedSeqAddressable(mutable.ArraySeq.fill(length.toInt)(0.asInstanceOf[Byte]), base)
+class ROM(seq: immutable.IndexedSeq[Byte], val base: Long) extends Addressable:
+  require(base >= 0, "base is negative")
+  require(seq.nonEmpty, "Addressable is empty")
+
+  val size = seq.length
+
+  def readByte(addr: Long): Int =
+    require(base <= addr && addr < base + size, "address out of range")
+    seq((addr - base).toInt) & 0xff
+
+  def writeByte(addr: Long, data: Byte): Unit = sys.error("not writable")
--- shared/src/main/scala/io/github/edadma/m88k/Main.scala
+++ shared/src/main/scala/io/github/edadma/m88k/Main.scala
@@ -1,7 +1,7 @@
 package io.github.edadma.m88k
 
 @main def run(): Unit =
-  val a = RAM(0, 10)
+  val a = new RAM(0, 10)
 
   a.writeByte(5, 123)
   println(a.readByte(5))
