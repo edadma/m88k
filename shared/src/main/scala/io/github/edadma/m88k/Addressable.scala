5ede2cd214b5c403184c08140799fa0c8a07c026
Edward Adrian Maxedon <edadma@gmail.com>
2023-05-20 13:14:21 -0400

    IndexedSeqAddressable

--- shared/src/main/scala/io/github/edadma/m88k/Addressable.scala
+++ shared/src/main/scala/io/github/edadma/m88k/Addressable.scala
@@ -1,12 +1,13 @@
 package io.github.edadma.m88k
 
 import scala.collection.immutable.ArraySeq
+import scala.collection.mutable
 
 trait Addressable:
   def base: Long
   def length: Long
   def readByte(addr: Long): Int
-  def writeByte(addr: Long, data: Int): Unit
+  def writeByte(addr: Long, data: Byte): Unit
 
 class Memory(blocks: Addressable*) extends Addressable:
   require(blocks.nonEmpty, "memory must contain at least one Addressable block")
@@ -28,4 +29,18 @@ class Memory(blocks: Addressable*) extends Addressable:
 
   def readByte(addr: Long): Int = block(addr) getOrElse badAddress readByte addr
 
-  def writeByte(addr: Long, data: Int): Unit = block(addr) getOrElse badAddress writeByte (addr, data)
+  def writeByte(addr: Long, data: Byte): Unit = block(addr) getOrElse badAddress writeByte (addr, data)
+
+class IndexedSeqAddressable(seq: IndexedSeq[Byte], val base: Long) extends Addressable:
+  require(base >= 0, "base is negative")
+  require(seq.nonEmpty, "Addressable is empty")
+
+  val length = seq.length
+
+  def readByte(addr: Long): Int =
+    require(base <= addr && addr < base + length, "address out of range")
+    seq((addr - base).toInt) & 0xff
+
+  def writeByte(addr: Long, data: Byte): Unit =
+    require(base <= addr && addr < base + length, "address out of range")
+    seq.asInstanceOf[mutable.IndexedSeq[Byte]]((addr - base).toInt) = data
