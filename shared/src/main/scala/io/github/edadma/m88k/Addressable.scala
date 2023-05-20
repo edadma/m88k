9de3b6d2b41efe2fec6c27482ff224ccadd67259
Edward Adrian Maxedon <edadma@gmail.com>
2023-05-20 12:55:26 -0400

    Memory

--- shared/src/main/scala/io/github/edadma/m88k/Addressable.scala
+++ shared/src/main/scala/io/github/edadma/m88k/Addressable.scala
@@ -4,12 +4,28 @@ import scala.collection.immutable.ArraySeq
 
 trait Addressable:
   def base: Long
-  def length: Int
+  def length: Long
   def readByte(addr: Long): Int
   def writeByte(addr: Long, data: Int): Unit
 
-//class Memory(blocks: Addressable*) extends Addressable:
-//  val mem = blocks.sorted.to(ArraySeq)
-//
-//  def block(addr: Long): Option[Addressable] =
-//    search(mem, addr)
+class Memory(blocks: Addressable*) extends Addressable:
+  require(blocks.nonEmpty, "memory must contain at least one Addressable block")
+
+  val mem = blocks.sortBy(_.base).to(ArraySeq)
+  val base = mem.head.base
+  val length = mem.last.base + mem.last.length - base
+
+  def block(addr: Long): Option[Addressable] =
+    search(mem, addr, _ < _.base, _ == _.base) match
+      case Left(idx) if idx == 0 => None
+      case Left(idx) =>
+        val prec = mem(idx - 1)
+
+        if addr < prec.base + prec.length then Some(prec) else None
+      case Right(idx) => Some(mem(idx))
+
+  private def badAddress: Nothing = sys.error("address not found")
+
+  def readByte(addr: Long): Int = block(addr) getOrElse badAddress readByte addr
+
+  def writeByte(addr: Long, data: Int): Unit = block(addr) getOrElse badAddress writeByte (addr, data)
