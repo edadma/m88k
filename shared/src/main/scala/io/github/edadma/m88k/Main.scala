40c80e555c20d1ff3574e85993c6d1a6ab8959c4
Edward Adrian Maxedon <edadma@gmail.com>
2023-05-20 12:36:58 -0400

    binary search

--- shared/src/main/scala/io/github/edadma/m88k/Main.scala
+++ shared/src/main/scala/io/github/edadma/m88k/Main.scala
@@ -1,7 +1,7 @@
 package io.github.edadma.m88k
 
 @main def run(): Unit =
-  println(search(IndexedSeq(3, 5), 5, _ < _))
-  println(search(IndexedSeq(C(3), C(5)), 5, (x, y) => x < y.a))
+  println(search(IndexedSeq(3, 5), 4, _ < _, _ == _))
+  println(search(IndexedSeq(C(3), C(5)), 4, _ < _.a, _ == _.a))
 
 case class C(a: Int)
--- shared/src/main/scala/io/github/edadma/m88k/search.scala
+++ shared/src/main/scala/io/github/edadma/m88k/search.scala
@@ -1,6 +1,6 @@
 package io.github.edadma.m88k
 
-def search[T, E](seq: IndexedSeq[T], elem: E, lt: (E, T) => Boolean): Either[Int, Int] =
+def search[T, E](seq: IndexedSeq[T], elem: E, lt: (E, T) => Boolean, eq: (E, T) => Boolean): Either[Int, Int] =
   def search(low: Int, high: Int): Either[Int, Int] =
     // If element not found
     if (low > high) Left(low) // return the insertion point
@@ -9,15 +9,15 @@ def search[T, E](seq: IndexedSeq[T], elem: E, lt: (E, T) => Boolean): Either[Int
       var middle = low + (high - low) / 2
 
       // If element found
-      if (seq(middle) == elem) {
-        return Right(middle)
+      if eq(elem, seq(middle)) then
+        Right(middle)
         // If element is before middle element
-      } else if (lt(elem, seq(middle)))
+      else if lt(elem, seq(middle)) then
         // Searching in the left half
         search(low, middle - 1)
       else
         // Searching in the right half
         search(middle + 1, high)
 
-  search(0, seq.length)
+  search(0, seq.length - 1)
 end search
